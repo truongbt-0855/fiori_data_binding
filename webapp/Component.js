8f7ec989bc968d9d000e95d5ac3d80768b2c483a
truongbt <bui.tuan.truong@sun-asterisk.com>
2025-04-16 09:57:49 +0700

    Step 4 + 5: Two-way binding. Change to OneWay binding

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,12 +1,22 @@
 sap.ui.define([
-    'sap/ui/core/UIComponent'
-], (UIComponent) => {
+    'sap/ui/core/UIComponent',
+    "sap/ui/model/BindingMode"
+], (UIComponent, BindingMode) => {
     'use strict';
 
     return UIComponent.extend('ui5.databinding.Component', {
         metadata: {
             interfaces: ['sap.ui.core.IAsyncContentCreation'],
             manifest: 'json'
+        },
+
+        /**
+         * @override
+         */
+        init() {
+            UIComponent.prototype.init.apply(this, arguments);
+            
+            this.getModel('data_json').setDefaultBindingMode(BindingMode.OneWay);
         }
     })
 })
