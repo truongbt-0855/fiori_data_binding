a3874dbe6e41e5ac1d660ea6d2fee3c7e06b7838
truongbt <bui.tuan.truong@sun-asterisk.com>
2025-04-16 16:51:54 +0700

    Step 12: Aggregation Biding Using templates

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,18 +1,25 @@
 sap.ui.define([
     "sap/m/library",
-    "sap/ui/core/mvc/Controller"
-], (mobileLibrary, Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/type/Currency"
+], (mobileLibrary, Controller, Currency) => {
     "use strict";
 
     return Controller.extend("ui5.databinding.controller.App", {
         formatMail(sFirstName, sLastName) {
             const oBundle = this.getView().getModel('data_i18n').getResourceBundle();
-            
+
             return mobileLibrary.URLHelper.normalizeEmail(
                 `${sFirstName}.${sLastName}@example.com`,
                 oBundle.getText('mailSubject', [sFirstName]),
                 oBundle.getText('mailBody')
             )
         },
+
+        formatStockValue(fUnitPrice, iStockLevel, sCurrCode) {
+            const oCurrency = new Currency();
+
+            return oCurrency.formatValue([fUnitPrice * iStockLevel, sCurrCode], 'string')
+        }
     });
 });
